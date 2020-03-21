c9ad44c98d7ff314e1010f7df72fce8da9051467
Shirley <shirleysensible@gmail.com>
2020-03-22 03:03:45 +0800

    the usage of vector<pair<,>>

--- Power_Value.cpp
+++ Power_Value.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int getKth(int lo, int hi, int k) {
-        int num=hi-lo+1;
-        list <int>PS;
-        list <int>Order;
-        vector <int>power;
-        vector <int>order;
+        vector <pair<int,int>>order;
         for(int i=lo;i<=hi;i++){
             int count=0,tmp=i;
             while(1){
                 if(tmp==1){
-                    order.push_back(count);
-                    power.push_back(i);
+                    order.push_back({count,i});
                     break;
                 }
                 else{
@@ -27,42 +22,7 @@ public:
             }
         }
         //compare
-        for(int i=0;i<num;i++){
-            if(i==0){
-                PS.push_back(power[i]);
-                Order.push_back(order[i]);
-            }
-            else if(i==1){
-                if(order[i]<Order[0]){
-                    Order.push_front(order[i]);
-                    PS.push_front(power[i]);
-                }
-                else{
-                    Order.push_back(order[i]);
-                    PS.push_back(power[i]);
-                }
-            }
-            else{
-                for(int j=1;j<i;j++){
-                    if(order[i]<Order[j]&&order[i]>Order[j-1])
-                        Order.increase()
-                }
-            }
-        }
-        
-        for(int j=0;j<k;j++){
-            for(int i=lo;i<hi+1;i++){
-                
-                if(O[i]==order[j]){
-                    if(j==k-1){
-                        num=i;
-                        break;
-                    }
-                    else
-                        O[i]=0;
-                }
-            }
-        }
-        return num;
+        sort(order.begin(),order.end());
+        return order[k-1].second;
     }
 };
