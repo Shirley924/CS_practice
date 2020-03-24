2163329af49d3bfcb6d7d41e41641a811be079ca
Shirley <shirleysensible@gmail.com>
2020-03-25 00:00:17 +0800

    AtCoder Beginner Contest 159

---
+++ Max_Volumn.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+  
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  double L;
+  cin>>L;
+  double die_length=L/3.0;
+  cout<<pow(die_length,3.0)<<endl;
+  
+  return 0;
+}
