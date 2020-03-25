91af0717b9a84820045a2d2a87b83820860b4e7b
Shirley <shirleysensible@gmail.com>
2020-03-26 00:09:27 +0800

    recursive

--- Staire.cpp
+++ Staire.cpp
@@ -3,20 +3,24 @@
 
 using namespace std;
 
+int way(int,int);
+
 int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	int N,k;
-	int arr[2000];
 	cin>>N,k;
 	
-
+	cout<<way(N,k)<<endl;
 
 	return 0;
 }
 
-int way(int N,int k,int *arr){
-	if(N==3)
-
-	return ;
+int way(int N,int k){
+	if(N<=2 && k==1)
+		return 1;
+	else if(N>2 && k>1)
+		return (way(N-1,k-1)+way(N-2,k-1))%(10^9+9);
+	else if(N>2 && k==1)
+		return 0;
 }
