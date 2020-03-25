a5ea7fb8a4cd4edd766373998be94ca77d1a28db
Shirley <shirleysensible@gmail.com>
2020-03-25 23:17:30 +0800

    NCKU Online Judge Contest#3-18

---
+++ Staire.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define endl '\n';
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int N,k;
+	int arr[2000];
+	cin>>N,k;
+	
+
+
+	return 0;
+}
+
+int way(int N,int k,int *arr){
+	if(N==3)
+
+	return ;
+}
