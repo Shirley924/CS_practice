d2ff7a91716480c220adfd4c4f47ff86d7e77559
Shirley <shirleysensible@gmail.com>
2020-04-12 12:23:06 +0800

    algorithm::find

---
+++ find_substring.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> ans;
+        for(int i=0;i<words.size();i++){
+            for(int j=0;j<words.size();j++){
+                if(i!=j){
+                size_t found=words[j].find(words[i]);
+	                
+                    if (found != string::npos){
+                        ans.push_back(words[i]);
+                        break;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
